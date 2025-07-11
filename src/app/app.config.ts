19713047069c19132ba3af16ea4aa9d17a13d929
Olla <olla.allan997@gmail.com>
2025-07-11 13:50:57 +0400

    sec

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,7 +4,8 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { HttpClientModule } from '@angular/common/http';
 import { AppTranslateModule } from './shared/modules/app-translate.module';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), importProvidersFrom(HttpClientModule), importProvidersFrom(AppTranslateModule.forRoot())]
+  providers: [provideRouter(routes), { provide: LocationStrategy, useClass: HashLocationStrategy }, importProvidersFrom(HttpClientModule), importProvidersFrom(AppTranslateModule.forRoot())]
 };
