d58d10630da0f7c753118d033d007cbe5baa55e7
Olla <olla.allan997@gmail.com>
2025-07-11 23:15:01 +0400

    lov3

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,8 +1,10 @@
 import { NgStyle } from '@angular/common';
 import { Component, inject } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { SharedModule } from '../../shared/shared.module';
 import { LanguageService } from '../../shared/services/language.service';
+import { CompaniesService } from '../../shared/services/companies.service';
+import { ICompany } from '../../app.component.models';
 
 @Component({
   selector: 'app-footer',
@@ -18,10 +20,24 @@ export class FooterComponent {
   tel: number = +97142570022;
   Fax: number = +97142570033;
   Mod: number = +971508877990
+  companiesServices = inject(CompaniesService);
+  companies: Array<ICompany> = [];
+
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
     this.languageService.lang$.subscribe(lang => {
       this.currentLang = lang
+    });
+
+    this.companies = this.companiesServices.getCompanies();
+  }
+
+  goToCompany(company: ICompany): void {
+    console.log(company.id)
+    const slug = company.title.toLowerCase().replace(/\s+/g, '-');
+    this.router.navigate(['companies', slug], {
+      state: { id: company.id }
     });
   }
 }
